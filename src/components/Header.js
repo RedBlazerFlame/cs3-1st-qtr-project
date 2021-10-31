9ef4270fecb68d4457469369aa060aa69fcd6842
RedBlazerFlame <gabeedevera@outlook.com>
2021-10-31 16:44:24 +0800

    Started work on the Header Component

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+const Header = (props) => {
+    return {
+        componentData: `
+        <a class="headerLeft" href="/about">
+            <img class="headerIcon" src="/images/logo1.png" alt="Site Icon" title="Made by Gabee De Vera">
+            <p class="title">
+                The Climate Post
+            </p>
+        </a>
+        <div class="searchbar">
+            <input id="headerSearchbarInput" placeholder="Search for Articles"/>
+            <img id="searchBarIcon" src="/images/magnifyingGlass1.svg" alt="Search Bar Icon" title="Made by Gabee De Vera"/>
+        </div>
+        `,
+        callbacks: {},
+        state: props,
+    };
+};
+export default Header;
