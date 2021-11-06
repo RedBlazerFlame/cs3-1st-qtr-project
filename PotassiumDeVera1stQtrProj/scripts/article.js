ef16cbb498e1218a82a0a210721b41ee51099ec2
RedBlazerFlame <gabeedevera@outlook.com>
2021-11-06 12:53:06 +0800

    Added ImageHeader functionality for the article page
    In the article page, JavaScript fetches the article metadata and then uses it to render the header

---
+++ PotassiumDeVera1stQtrProj/scripts/article.js
@@ -0,0 +1,25 @@
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+import ImageHeader from "../components/ImageHeader.js";
+const urlParameters = new window.URLSearchParams(window.location.search);
+const articleId = urlParameters.get("id");
+const imageHeader = document.querySelector("section.imageHeader");
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const articlesJSON = yield fetch("/json/articles.json").then((res) => res.json());
+    const articleData = articlesJSON.articles[articleId];
+    imageHeader.innerHTML = ImageHeader({
+        image: {
+            src: articleData.banner.url,
+            alt: articleData.banner.alt,
+            title: articleData.banner.title,
+        },
+        showTitle: false,
+    }).componentData;
+}))();
---
+++ PotassiumDeVera1stQtrProj/scripts/article.ts
@@ -0,0 +1,30 @@
+import ImageHeader from "../components/ImageHeader.js";
+import { ArticlesJSON, ArticleData } from "./types.js";
+
+// Declaring Constants and Variables
+const urlParameters = new window.URLSearchParams(window.location.search);
+const articleId = urlParameters.get("id");
+
+// Getting a reference to HTML Elements
+const imageHeader: HTMLElement = document.querySelector("section.imageHeader");
+
+// Wrapping code in an async function so that we can await asynchronous responses
+(async () => {
+    // This will retrieve all of the article metadata
+    const articlesJSON: ArticlesJSON = await fetch("/json/articles.json").then(
+        (res) => res.json()
+    );
+
+    // This will retrieve the article data for the page
+    const articleData: ArticleData = articlesJSON.articles[articleId];
+
+    // Updating Image Header
+    imageHeader.innerHTML = ImageHeader({
+        image: {
+            src: articleData.banner.url,
+            alt: articleData.banner.alt,
+            title: articleData.banner.title,
+        },
+        showTitle: false,
+    }).componentData;
+})();
