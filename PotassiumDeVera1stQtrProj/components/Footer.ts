5bbd3a80909bd7a6fd0975bb0148a206b7b8cf11
Norman (22194) <gabeedevera@outlook.com>
2022-01-17 08:14:47 +0800

    Created Social Media Links

--- PotassiumDeVera1stQtrProj/components/Footer.ts
+++ PotassiumDeVera1stQtrProj/components/Footer.ts
@@ -12,6 +12,17 @@ const Footer: FC<FooterProps> = (props: FooterProps) => {
             </p>
         </div>
         <a class="aboutUsLink" href="/about">About Us</a>
+        <div class="accounts">
+            <div class="clickableIcon">
+                <a title="Email" href="mailto:redblazerflame@gmail.com"> </a>
+            </div>
+            <div class="clickableIcon">
+                <a title="GitHub" href="https://github.com/RedBlazerFlame"> </a>
+            </div>
+            <div class="clickableIcon">
+                <a title="Facebook" href="https://www.facebook.com/eleogen.devera"> </a>
+            </div>
+        </div>
         <div class="copyright">Copyright &copy; Gabee De Vera</div>
         `,
         callbacks: {},
